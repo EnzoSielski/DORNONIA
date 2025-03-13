8e4c2d804122dc3b95c1188d009a326834b894ec
EnzoSielski <141641626+EnzoSielski@users.noreply.github.com>
2025-03-13 09:09:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener(DOMContentLoaded, function() {
-    const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const aumentaFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoFonteAtual = 1;
 
-    
+    aumentaFonteBotao.addEventListener('click', function() {   
+        TamanhoAtualFonte += 0.1;
+        document.body.style.fontsize = '$(tamanhoAtualFonte)rem';
 
+    });
+
+    diminuiFonteBotao.addEventListener('click', function() {   
+        TamanhoAtualFonte -= 0.1;
+        document.body.style.fontsize = '$(tamanhoAtualFonte)rem';
+
+    });
 })
