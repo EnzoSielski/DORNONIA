b24e4a4f9a7b2d59d8735eafd6c0a63f27f46cc6
EnzoSielski <141641626+EnzoSielski@users.noreply.github.com>
2025-04-03 09:01:59 -0300

    .

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+document.addEventListener("DOMContentLoaded", function() {
+    const aumentaFonteBotao = document.getElementById("aumentar-fonte");
+    const diminuiFonteBotao = document.getElementById("diminuir-fonte");
 
     let TamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function() {   
+    aumentaFonteBotao.addEventListener("click", function() {   
         TamanhoAtualFonte += 0.1;
-        document.body.style.fontsize = '$(TamanhoAtualFonte)rem';
-        document.h2.style.fontsize = '$(TamanhoAtualFonte)rem';
+        document.body.style.fontsize = "$(TamanhoAtualFonte)rem";
+        document.h2.style.fontsize = "$(TamanhoAtualFonte)rem";
 
     });
 
-    diminuiFonteBotao.addEventListener('click', function() {   
+    diminuiFonteBotao.addEventListener("click", function() {   
         TamanhoAtualFonte -= 0.1;
-        document.body.style.fontsize = '$(TamanhoAtualFonte)rem';
-        document.h2.style.fontsize = '$(TamanhoAtualFonte)rem';
+        document.body.style.fontsize = "$(TamanhoAtualFonte)rem";
+        document.h2.style.fontsize = "$(TamanhoAtualFonte)rem";
     });
 })
