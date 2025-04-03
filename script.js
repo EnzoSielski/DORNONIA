3c454d95c6cf4a39462af49604975c7c3ebdd4cf
EnzoSielski <enzo.sielski@escola.pr.gov.br>
2025-04-03 18:07:22 -0300

    .

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const aumentaFonteBotao = document.getElementById("aumentar-fonte");
-    const diminuiFonteBotao = document.getElementById("diminuir-fonte");
-
-    let TamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener("click", function() {   
-        TamanhoAtualFonte += 0.5;
-        document.style.body.fontsize = "$(TamanhoAtualFonte)rem";
-        document.style.h2.fontsize = "$(TamanhoAtualFonte)rem";
-
-    });
-
-    diminuiFonteBotao.addEventListener("click", function() {   
-        TamanhoAtualFonte -= 0.5;
-        document.style.body.fontsize = "$(TamanhoAtualFonte)rem";
-        document.style.h2.fontsize = "$(TamanhoAtualFonte)rem";
-    });
-})
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opc-acessibilidade')
+ 
+    botaoDeAcessibilidade.addEventListener('click', function (){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+ 
+     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+   
+    })
+ 
+     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+     
+     const alternaContraste = document.getElementById('alterna-contraste')
+ 
+     let tamanhoAtualFonte = 1;
+ 
+     aumentaFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte += 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+         document.h2.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
+     })
+ 
+     diminuiFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte -= 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+         document.h2.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
+     })
+ 
+     alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+     })
+ 
+ 
+ })
+ 
+ ScrollReveal().reveal('#inicio', { delay: 500 });
+ ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#galeria', { delay: 500 });
+ ScrollReveal().reveal('#contato', { delay: 500 });
