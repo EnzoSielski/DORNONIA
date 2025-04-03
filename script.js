253b7f471411ad7e51acebc3831f87280c0ee4e6
EnzoSielski <141641626+EnzoSielski@users.noreply.github.com>
2025-04-03 08:57:30 -0300

    .

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    let TamanhoFonteAtual = 1;
+    let TamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function() {   
         TamanhoAtualFonte += 0.1;
